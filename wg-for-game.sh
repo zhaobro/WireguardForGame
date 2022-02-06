8d3f4b61482cfc3b6072fd29325ced08af92617a
J. S <81501795+zhaobro@users.noreply.github.com>
2022-02-06 20:50:23 +0800

    port change

--- wg-for-game.sh
+++ wg-for-game.sh
@@ -117,16 +117,16 @@ EOF
 auto_start(){
     echo -e "${Green}正在配置加速程序开机自启${Font}"
     nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1195 -f2:4 --mode 0 --timeout 2 >speeder.log 2>&1 &
-	nohup ./udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &
+	nohup ./udp2raw -s -l0.0.0.0:10789 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 &
     if [ "${OS}" == 'CentOS' ];then
         sed -i '/exit/d' /etc/rc.d/rc.local
         echo "nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1195 -f2:4 --mode 0 --timeout 2 >speeder.log 2>&1 &
-nohup ./udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.d/rc.local
+nohup ./udp2raw -s -l0.0.0.0:10789 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.d/rc.local
         chmod +x /etc/rc.d/rc.local
     elif [ -s /etc/rc.local ]; then
         sed -i '/exit/d' /etc/rc.local
         echo "nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1195 -f2:4 --mode 0 --timeout 2 >speeder.log 2>&1 &
-nohup ./udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.local
+nohup ./udp2raw -s -l0.0.0.0:10789 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.local
         chmod +x /etc/rc.local
     else
 echo -e "${Green}检测到系统无rc.local自启，正在为其配置... ${Font} "
@@ -159,7 +159,7 @@ echo "#!/bin/sh -e
 # By default this script does nothing.
 " > /etc/rc.local
 echo "nohup ./speederv2 -s -l0.0.0.0:9999 -r127.0.0.1:1195 -f2:4 --mode 0 --timeout 2 >speeder.log 2>&1 &
-nohup ./udp2raw -s -l0.0.0.0:9898 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.local
+nohup ./udp2raw -s -l0.0.0.0:10789 -r 127.0.0.1:9999  --raw-mode faketcp  -a -k passwd >udp2raw.log 2>&1 & " >> /etc/rc.local
 chmod +x /etc/rc.local
 systemctl enable rc-local >/dev/null 2>&1
 systemctl start rc-local >/dev/null 2>&1
